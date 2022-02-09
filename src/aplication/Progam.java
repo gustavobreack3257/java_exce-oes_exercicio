3abf19e3d101a070ab9c334711f7c671e79d5be6
Gustavo costa <guh-51@hotmail.com>
2022-02-09 11:36:56 -0300

    Soluçao própria desenvolvendo...

--- src/aplication/Progam.java
+++ src/aplication/Progam.java
@@ -1,10 +1,32 @@
 package aplication;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account;
+
 public class Progam {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Locale.setDefault(Locale.US);	
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Entre com dados da conta:");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		System.out.println("holder: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Initial balance: ");
+		Double balance = sc.nextDouble();
+		System.out.print("Withdraw Limit: ");
+		Double withdrawLimit = sc.nextDouble();
+		Account account = new Account(number, holder, balance, withdrawLimit);
+		
+		System.out.println("Enter amount for withdraw: ");
+		Double saque = sc.nextDouble();
+		
+		sc.close();
 	}
 
 }
